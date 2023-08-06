ce8d3af898120bd47a16d9d6b6cab57580d8c653
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-08-06 14:31:46 +0000

    fixed path

--- mood_classification/deam_organized/created_sorted_directories.py
+++ mood_classification/deam_organized/created_sorted_directories.py
@@ -3,9 +3,9 @@ import csv
 import shutil
 
 # Replace these with the actual file paths and directory paths
-csv_file = 'arousal_extracted.csv'
-mp3_directory = '../raw_dataset/deam-dataset/DEAM_audio/MEMD_audio'
-output_directory = 'arousal_dataset'
+csv_file = 'valence_extracted.csv'
+mp3_directory = '../raw-dataset/deam-dataset/DEAM_audio/MEMD_audio'
+output_directory = 'valence_dataset'
 
 # Create the output directory if it doesn't exist
 if not os.path.exists(output_directory):
@@ -16,12 +16,12 @@ with open(csv_file, 'r') as file:
     reader = csv.DictReader(file)
     for row in reader:
         song_id = row['song_id']
-        arousal_mean = row['arousal_mean']
+        valence_mean = row['valence_mean']
 
         # Create a directory for the specific arousal/valence_mean if it doesn't exist
-        arousal_directory = os.path.join(output_directory, f'arousal_{arousal_mean}')
-        if not os.path.exists(arousal_directory):
-            os.makedirs(arousal_directory)
+        valence_directory = os.path.join(output_directory, f'valence_{valence_mean}')
+        if not os.path.exists(valence_directory):
+            os.makedirs(valence_directory)
 
         # Get the source mp3 file path
         mp3_file_path = os.path.join(mp3_directory, f'{song_id}.mp3')
@@ -29,7 +29,7 @@ with open(csv_file, 'r') as file:
         # Check if the mp3 file exists
         if os.path.exists(mp3_file_path):
             # Copy the mp3 file to the corresponding arousal/valence directory
-            destination_file_path = os.path.join(arousal_directory, f'{song_id}.mp3')
+            destination_file_path = os.path.join(valence_directory, f'{song_id}.mp3')
             shutil.copyfile(mp3_file_path, destination_file_path)
 
-print("MP3 files copied to directories based on arousal_mean.")
+print("MP3 files copied to directories based on valence_mean.")
