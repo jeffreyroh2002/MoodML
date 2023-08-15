bd19af47eed24ccc332188eb52f4588aec875f33
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-08-15 06:11:11 +0000

    added prediction for arousal as well

--- testing_audio/predict_russell.py
+++ testing_audio/predict_russell.py
@@ -4,7 +4,8 @@ from tensorflow import keras
 import os
 
 # Load the saved model
-saved_model_path = "../mood_classification/results/814_PCRNN_2D_valence_50each/saved_model"
+arousal_model_path = "../mood_classification/results/812_PCRNN_2D_arousal_50each/saved_model"
+valence_model_path = "../mood_classification/results/814_PCRNN_2D_valence_50each/saved_model"
 test_data_path = "test_audio_data_arousal.json"
 
 def load_testing_data(test_data_path):
@@ -19,16 +20,28 @@ def load_testing_data(test_data_path):
 X_test, y_test = load_testing_data(test_data_path)
 X_test = X_test[..., np.newaxis]  # If needed, reshape your data for the model input
 
-loaded_model = keras.models.load_model(saved_model_path)
+loaded_arousal_model = keras.models.load_model(arousal_model_path)
+loaded_valence_model = keras.models.load_model(valence_model_path)
 
 # Make predictions
-predictions = loaded_model.predict(X_test)
+a_predictions = loaded_arousal_model.predict(X_test)
+v_predictions = loaded_valence_model.predict(X_test)
 
 # If you have a classification task, you can get the predicted class indices:
-predicted_class_indices = np.argmax(predictions, axis=1)
+a_predicted_class_indices = np.argmax(a_predictions, axis=1)
+v_predicted_class_indices = np.argmax(v_predictions, axis=1)
 
 # Define your label list mapping class indices to labels
-label_list = {
+a_label_list = {
+    0: "Arousal_2",
+    1: "Arousal_3",
+    2: "Arousal_4",
+    3: "Arousal_5",
+    4: "Arousal_6",
+    5: "Arousal_7"
+}
+
+v_label_list = {
     0: "Valence_2",
     1: "Valence_3",
     2: "Valence_4",
@@ -39,8 +52,11 @@ label_list = {
 }
 
 # Assuming label_list contains the mapping of class indices to labels
-predicted_labels = [label_list[index] for index in predicted_class_indices]
-for i, label in enumerate(predicted_labels):
-    print(f"Sample {i + 1}: Predicted Label: {label}")
 
-print(predicted_labels)
+a_predicted_labels = [a_label_list[index] for index in a_predicted_class_indices]
+for i, label in enumerate(a_predicted_labels):
+    print(f"Sample {i + 1}: Arousal Predicted Label: {label}")
+
+v_predicted_labels = [v_label_list[index] for index in v_predicted_class_indices]
+for i, label in enumerate(v_predicted_labels):
+    print(f"Sample {i + 1}: Valence Predicted Label: {label}")
