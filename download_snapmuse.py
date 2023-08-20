d29e736dec4721ba781eba55d8a02fb0f6209188
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-08-20 12:50:26 +0000

    selenium webdriver

--- download_snapmuse.py
+++ download_snapmuse.py
@@ -1,36 +1,39 @@
+from selenium import webdriver
 import os
-import requests
 
 # Define the moods and the number of audio files to download for each mood
 moods = ['Upbeat', 'Happy', 'Romantic', 'Sad', 'Calm', 'Chill', 'Dreamy', 'Relax', 'Energetic', 'Angry', 'Tense', 'Groovy']  # Add more moods as needed
-num_files_per_mood = 15
+num_files_per_mood = 10
 
 # Specify the directory where you want to save the downloaded files
-download_directory = 'SnapMuse_dataset'
+download_directory = 'audio_files'
 os.makedirs(download_directory, exist_ok=True)
 
-# Base URL for the audio files
-base_url = 'https://snapmuse.com/moods/'
+# Initialize the web driver (replace 'chromedriver.exe' with the path to your web driver executable)
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
 
 # Loop through each mood
 for mood in moods:
     print(f"Downloading {num_files_per_mood} files for '{mood}' mood...")
-    
+
     # Create a subdirectory for the current mood
     mood_directory = os.path.join(download_directory, mood)
     os.makedirs(mood_directory, exist_ok=True)
+
+    # Visit the mood's URL
+    driver.get(f"https://snapmuse.com/moods/{mood}")
     
-    # Download the audio files
+    # Locate and click the audio file download links
     for i in range(1, num_files_per_mood + 1):
-        file_url = f"{base_url}{mood}/{mood}-{i}.mp3"
-        response = requests.get(file_url)
-        
-        if response.status_code == 200:
-            file_path = os.path.join(mood_directory, f"{mood}-{i}.mp3")
-            with open(file_path, 'wb') as f:
-                f.write(response.content)
-            print(f"Downloaded {file_path}")
-        else:
-            print(f"Failed to download {file_url}")
-            
+        try:
+            # Modify the file extension from .mp3 to .wav in the XPath
+            download_button = driver.find_element_by_xpath(f"//a[contains(text(), '{mood}-{i}.wav')]")
+            download_button.click()
+            print(f"Downloaded {mood}-{i}.wav")
+        except:
+            print(f"Failed to download {mood}-{i}.wav")
+
 print("Download complete!")
+
+# Close the web driver
+driver.quit()
