2c7b9e3bdbc47f91f3d4a295aa55d42c02711805
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-09-02 04:11:05 +0000

    added radar chart output

--- snapmuse_7_test/predict_mood.py
+++ snapmuse_7_test/predict_mood.py
@@ -1,11 +1,16 @@
 import json
 import numpy as np
 from tensorflow import keras
+import plotly.graph_objs as go
 import os
 
 # Load the saved model
 saved_model_path = "../mood_classification/results/901_PCRNN_2D_snapmuse_7/saved_model"
 test_data_path = "real_data_3sec.json"
+output_dir = "radar_charts"  # Directory to save individual radar chart images
+
+# Create the output directory if it doesn't exist
+os.makedirs(output_dir, exist_ok=True)
 
 def load_testing_data(test_data_path):
     with open(test_data_path, "r") as fp:
@@ -38,34 +43,61 @@ label_list = {
     6: "Uplifting"
 }
 
-# Initialize variables to keep track of counts
+# Initialize variables for percentage calculation
 segment_count = 0
 label_counts = {label: 0 for label in label_list.values()}
+song_radar_values = np.zeros(len(label_list))
+song_index = 0
 
 # Iterate through predicted labels
 for i, label_index in enumerate(predicted_class_indices):
     label = label_list[label_index]
+    
+    # Update label counts for percentage calculation
     label_counts[label] += 1
     segment_count += 1
 
-    # Calculate and print percentages every 60 segments
+    # Update radar values for the current song
+    song_radar_values[label_index] += 1
+
+    # Calculate and save the radar chart when reaching the end of a song (every 60 segments)
     if segment_count == 60:
-        print("Percentage of each label:")
-        for label, count in label_counts.items():
-            percentage = (count / 60) * 100
-            print(f"{label}: {percentage:.2f}%")
-        print("\n")
+        # Calculate the average radar values for the song
+        avg_radar_values = song_radar_values / segment_count
+
+        # Output the averaged radar values to the terminal
+        print(f"Song {song_index + 1} Average Radar Values:")
+        for label, avg_value in zip(label_list.values(), avg_radar_values):
+            print(f"{label}: {avg_value:.2f}")
+
+        # Create a radar chart trace for the averaged values
+        radar_chart_trace = go.Scatterpolar(
+            r=avg_radar_values,
+            theta=list(label_list.values()),
+            fill='toself',
+            name=f"Song_{song_index + 1}_Average"
+        )
+
+        # Create a layout for the radar chart
+        layout = go.Layout(
+            polar=dict(
+                radialaxis=dict(showticklabels=False, ticks='', showline=False),
+                angularaxis=dict(showticklabels=True, ticks='outside', showline=True)
+            ),
+            showlegend=True
+        )
+
+        # Create a figure with the radar chart for the song
+        fig = go.Figure(data=[radar_chart_trace], layout=layout)
+
+        # Save the figure as an image (PNG) in the output directory
+        output_filename = os.path.join(output_dir, f"Song_{song_index + 1}_RadarChart.png")
+        fig.write_image(output_filename)
+
+        # Reset variables for the next song
         segment_count = 0
         label_counts = {label: 0 for label in label_list.values()}
+        song_radar_values = np.zeros(len(label_list))
+        song_index += 1
 
-# Print the remaining percentages if there are fewer than 60 segments
-if segment_count > 0:
-    print("Percentage of each label for the remaining segments:")
-    for label, count in label_counts.items():
-        percentage = (count / segment_count) * 100
-        print(f"{label}: {percentage:.2f}%")
-
-# Assuming label_list contains the mapping of class indices to labels
-predicted_labels = [label_list[index] for index in predicted_class_indices]
-for i, label in enumerate(predicted_labels):
-    print(f"Sample {i + 1}: Predicted Label: {label}")
+# You can perform additional actions or analysis with the generated radar charts as needed.
