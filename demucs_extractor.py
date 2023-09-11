4899ad1677c3f3a3a7de0735a262f90555ba83bd
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-09-11 12:07:27 +0000

    save processing time and disk space

--- demucs_extractor.py
+++ demucs_extractor.py
@@ -14,18 +14,13 @@ os.makedirs(os.path.join(output_directory, 'angry_background'), exist_ok=True)
 # List all audio files in the input directory
 audio_files = [f for f in os.listdir(audio_directory) if f.endswith('.wav')]
 
-audio_files = [f for f in os.listdir(audio_directory) if f.endswith('.wav')]
-
-# Loop through each audio file and perform source separation for vocals and background
+# Loop through each audio file and perform source separation
 for audio_file in audio_files:
     # Construct the full path to the input audio file
     input_audio_path = os.path.join(audio_directory, audio_file)
 
-    # Perform source separation with Demucs to extract vocals
-    subprocess.run(['demucs', '--two-stems=vocals', input_audio_path])
-
-    # Perform source separation with Demucs to extract background (other)
-    subprocess.run(['demucs', '--two-stems=other', input_audio_path])
+    # Perform source separation with Demucs to extract all four sources
+    subprocess.run(['demucs', input_audio_path])
 
     # Move the separated vocals and background files to their respective directories
     vocals_file = os.path.join('separated', 'hdemucs', audio_file.replace('.wav', ''), 'vocals.wav')
@@ -37,5 +32,8 @@ for audio_file in audio_files:
     os.rename(vocals_file, os.path.join(output_vocals_directory, audio_file.replace('.wav', '_vocals.wav')))
     os.rename(background_file, os.path.join(output_background_directory, audio_file.replace('.wav', '_background.wav')))
 
-# Clean up the 'separated' directory created by Demucs
-subprocess.run(['rm', '-r', 'separated'])
+    # Clean up the 'separated' directory (remove drums.wav and bass.wav)
+    subprocess.run(['rm', os.path.join('separated', 'hdemucs', audio_file.replace('.wav', ''), 'drums.wav')])
+    subprocess.run(['rm', os.path.join('separated', 'hdemucs', audio_file.replace('.wav', ''), 'bass.wav')])
+
+print('Extraction completed.')
