8e1524cf39d685eef4cf1f0f817ae9809986c5d8
Jaehwi-Song <wotns0319@naver.com>
2023-09-15 10:40:23 +0000

    testing yt dataset with model trained with background musics

--- yt_dataset_testing/predict_mood.py
+++ yt_dataset_testing/predict_mood.py
@@ -5,10 +5,10 @@ import plotly.graph_objs as go
 import os
 
 # Load the saved model
-saved_model_path = "../mood_classification/results/909_PCRNN_2D_binary_valence_dataset_with_lyrics_3sec/saved_model"
+saved_model_path = "../mood_classification/results/914_PCRNN_2D_lyrics_reduced_lr/saved_model"
 test_data_path = "p4_8songs_3sec.json"
-model_saved_mfcc = "../binary_classification/json_files/valence_dataset_lyrics.json"
-output_dir = "new_radar_results/new_radar_909p2_valence"  # Directory to save individual radar chart images
+model_saved_mfcc = "../dataset_lyrics_extracted/background_dataset.json"
+output_dir = "new_radar_results/new_radar_915_background"  # Directory to save individual radar chart images
 
 # Create the output directory if it doesn't exist
 os.makedirs(output_dir, exist_ok=True)
@@ -63,56 +63,47 @@ for i, label in enumerate(predicted_class_indices):
 segment_count = 0
 label_counts = {label: 0 for label in label_list.values()}
 song_radar_values = np.zeros(len(label_list))
-prev_song_title = Sorted_Song_list[0][:-7]
+song_index = 0
 
 for f in Sorted_Song_list:
     predicted_idx = Song_list[f]
 
     for idx in predicted_idx:
-        f_name = f[:-7]
-        if f_name != prev_song_title:
-            # Calculate the average radar values for the song
-            avg_radar_values = song_radar_values / segment_count
-            # Output the averaged radar values to the terminal
-            print(f"{f_name} Average Radar Values:")
-            for label, avg_value in zip(label_list.values(), avg_radar_values):
-                print(f"{label}: {avg_value:.2f}")
-            
-            radar_chart_trace = go.Scatterpolar(
+        label = label_list[predicted_class_indices[idx]]
+
+        # Update label counts for percentage calculation
+        label_counts[label] += 1
+        segment_count += 1
+        song_radar_values[predicted_class_indices[idx]] += 1
+
+    # Calculate the average radar values for the song
+    avg_radar_values = song_radar_values / segment_count
+    # Output the averaged radar values to the terminal
+    print(f"{f} Average Radar Values:")
+    for label, avg_value in zip(label_list.values(), avg_radar_values):
+        print(f"{label}: {avg_value:.2f}")
+
+    radar_chart_trace = go.Scatterpolar(
             r=avg_radar_values,
             theta=list(label_list.values()),
             fill='toself',
-            name=f"{f_name}_Average"
-            )
-            # Create a layout for the radar chart
-            layout = go.Layout(
+            name=f"{f}_Average"
+    )
+    # Create a layout for the radar chart
+    layout = go.Layout(
             polar=dict(
                 radialaxis=dict(showticklabels=False, ticks='', showline=False),
                 angularaxis=dict(showticklabels=True, ticks='outside', showline=True)
             ),
             showlegend=True
-            )
-
-            fig = go.Figure(data=[radar_chart_trace], layout=layout)
-
-            # Save the figure as an image (PNG) in the output directory
-            output_filename = os.path.join(output_dir, f"{f_name}_RadarChart.png")
-            fig.write_image(output_filename)
-
-            # Reset variables for the next song
-            segment_count = 0
-            label_counts = {label: 0 for label in label_list.values()}
-            song_radar_values = np.zeros(len(label_list))
-
-            prev_song_title = f_name
-            
-
-        label = label_list[predicted_class_indices[idx]]
-        
-        # Update label counts for percentage calculation
-        label_counts[label] += 1
-        segment_count += 1
-        # Update radar values for the current song
-        song_radar_values[predicted_class_indices[idx]] +=1
+    )
+    fig = go.Figure(data=[radar_chart_trace], layout=layout)
+    # Save the figure as an image (PNG) in the output directory
+    output_filename = os.path.join(output_dir, f"{f}_RadarChart.png")
+    fig.write_image(output_filename)
+    # Reset variables for the next song
+    segment_count = 0
+    label_counts = {label: 0 for label in label_list.values()}
+    song_radar_values = np.zeros(len(label_list))
 
 print(Song_list)
