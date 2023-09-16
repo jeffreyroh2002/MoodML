08cd4ed2c13ad922d93916b61864906fb78ba2dc
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-09-16 03:29:17 +0000

    changed path for testing data

--- etc_scripts/move_separated_files.py
+++ etc_scripts/move_separated_files.py
@@ -1,7 +1,7 @@
 import os
 import shutil
 
-FILE_PATH = "separated/htdemucs"
+FILE_PATH = "../yt_dataset_testing/yt_dataset/splited_files_extracted/htdemucs"
 
 vocal_files = []
 background_files = []
@@ -11,7 +11,7 @@ for root, dirs, files in os.walk(FILE_PATH):
         vocal = os.path.join(root, "vocals.wav")
         background = os.path.join(root, "no_vocals.wav")
         file_name = root.split('/')[-1]
-        shutil.move(vocal, "dataset_lyrics_extracted/Vocals/Bright/" + file_name + "_vocals.wav")
-        shutil.move(background, "dataset_lyrics_extracted/Background/Bright/" + file_name + "_no_vocals.wav")
+        shutil.move(vocal, "../yt_dataset_testing/yt_dataset/splited_files_extracted/" + file_name + "_vocals.wav")
+        shutil.move(background, "../yt_dataset_testing/yt_dataset/splited_files_extracted/" + file_name + "_no_vocals.wav")
 
 print("Files have successfully moved")
