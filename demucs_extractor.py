4ff3e0f47ce4c21ad72e6239f8b79ecee92e83e8
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-09-16 03:00:16 +0000

    fix error

--- demucs_extractor.py
+++ demucs_extractor.py
@@ -1,15 +1,8 @@
 import os
 import subprocess
-import shutil
 
 # Specify the parent directory containing subdirectories with WAV files
-parent_directory = '~/MoodML/yt_dataset_testing/yt_dataset/splited_file/'
-
-# Specify the output directory for extracted vocal files
-output_directory = '~/MoodML/yt_dataset_testing/yt_dataset/splited_file/extracted_vocals/'
-
-# Create the output directory if it doesn't exist
-os.makedirs(output_directory, exist_ok=True)
+parent_directory = 'yt_dataset_testing/yt_dataset/splited_file/'
 
 # Loop through subdirectories
 for subdir in os.listdir(parent_directory):
@@ -23,10 +16,5 @@ for subdir in os.listdir(parent_directory):
         for audio_file in audio_files:
             # Perform source separation with Demucs to extract vocals
             subprocess.run(['demucs', '--out', 'vocals', audio_file])
-
-            # Move the extracted vocal file to the output directory
-            vocal_file = os.path.splitext(os.path.basename(audio_file))[0] + '_vocals.wav'
-            vocal_path = os.path.join(subdir_path, vocal_file)
-            shutil.move(vocal_path, os.path.join(output_directory, vocal_file))
 
 print('Extraction of vocals completed.')
