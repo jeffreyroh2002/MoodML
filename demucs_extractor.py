0779ec767cf1a774c959130e12a4d0365ff5eada
Jaehwi-Song <wotns0319@naver.com>
2023-09-16 02:54:46 +0000

    fix prob

--- demucs_extractor.py
+++ demucs_extractor.py
@@ -1,18 +1,20 @@
 import os
 import subprocess
 
-# Specify the directory containing your WAV audio files
-audio_directory = 'yt_dataset_testing/yt_dataset/splited_file/'
+# Specify the parent directory containing subdirectories with WAV files
+parent_directory = 'yt_dataset_testing/yt_dataset/splited_file/'
 
-# List all audio files in the input directory
-audio_files = [f for f in os.listdir(audio_directory) if f.endswith('.wav')]
+# Loop through subdirectories
+for subdir in os.listdir(parent_directory):
+    subdir_path = os.path.join(parent_directory, subdir)
 
-# Loop through each audio file and perform source separation for vocals
-for audio_file in audio_files:
-    # Construct the full path to the input audio file
-    input_audio_path = os.path.join(audio_directory, audio_file)
+    if os.path.isdir(subdir_path):
+        # List all the audio files in the subdirectory
+        audio_files = [os.path.join(subdir_path, f) for f in os.listdir(subdir_path) if f.endswith('.wav')]
 
-    # Perform source separation with Demucs to extract vocals
-    subprocess.run(['demucs', '--two-stems=vocals', input_audio_path])
+        # Loop through each audio file and perform source separation for vocals
+        for audio_file in audio_files:
+            # Perform source separation with Demucs to extract vocals
+            subprocess.run(['demucs', '--out', 'vocals', audio_file])
 
 print('Extraction of vocals completed.')
