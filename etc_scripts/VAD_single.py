56d83f472ff302056c295f3c1ef406b9e236e1b3
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-09-25 12:21:56 +0000

    change to 3000 sec

--- etc_scripts/VAD_single.py
+++ etc_scripts/VAD_single.py
@@ -1,7 +1,7 @@
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
-def remove_empty_vocal(input_audio_path, output_audio_path, min_silence_duration=1000):
+def remove_empty_vocal(input_audio_path, output_audio_path, min_silence_duration=3000):
     try:
         # Load the input vocal audio file
         audio = AudioSegment.from_file(input_audio_path, format="wav")
@@ -27,6 +27,6 @@ if __name__ == "__main__":
     output_audio_path = "../vocal_timbre_analysis/yt_dataset/voice_cleaned/I-vocals-cleaned.wav"  # Specify the output file path
 
     # Specify the minimum duration of silence to consider as empty vocal (in milliseconds)
-    min_silence_duration = 1000  # Adjust this value as needed
+    min_silence_duration = 3000  # Adjust this value as needed
 
     remove_empty_vocal(input_audio_path, output_audio_path, min_silence_duration)
