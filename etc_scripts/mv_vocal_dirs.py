7c215b01e2116cad527beb560008e3b6824dbd60
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-09-25 11:19:55 +0000

    restructured

--- etc_scripts/mv_vocal_dirs.py
+++ etc_scripts/mv_vocal_dirs.py
@@ -1,29 +1,26 @@
 import os
 import shutil
 
-# Define the source directory
-source_dir = "../vocal_timbre_analysis/yt_dataset/urls/wav_files"
+# Define the source and destination directories
+source_dir = "../vocal_timbre_analysis/yt_dataset/urls/wav_files/ethereal_dreamy_wav"
+destination_dir = "../vocal_timbre_analysis/yt_dataset/urls/wav_files/eth_vocal"
 
-# Define the destination directory
-destination_dir = "../vocal_timbre_analysis/yt_dataset/voice_extracted"
+# Walk through the source directory
+for root, dirs, files in os.walk(source_dir):
+    for file in files:
+        # Check if the file is named "vocals.wav"
+        if file == "vocals.wav":
+            # Get the parent directory name
+            parent_dir_name = os.path.basename(root)
+            
+            # Create the new file name
+            new_file_name = f"{parent_dir_name}-vocals.wav"
+            
+            # Create the full source and destination paths
+            source_file_path = os.path.join(root, file)
+            destination_file_path = os.path.join(destination_dir, new_file_name)
+            
+            # Copy and rename the file
+            shutil.copy(source_file_path, destination_file_path)
 
-# Function to copy and rename files
-def copy_and_rename_files(src_dir, dest_dir):
-    for root, dirs, files in os.walk(src_dir):
-        for file in files:
-            if file == "vocals.wav":
-                # Get the parent directory name
-                parent_dir = os.path.basename(os.path.dirname(root))
-                # Create the new directory in the destination
-                new_dir = os.path.join(dest_dir, parent_dir)
-                os.makedirs(new_dir, exist_ok=True)
-                # Create the new file name
-                new_name = f"{parent_dir}-{file}"
-                old_path = os.path.join(root, file)
-                new_path = os.path.join(new_dir, new_name)
-                # Copy the file to the new location with the new name
-                shutil.copy(old_path, new_path)
-                print(f"Copied: {old_path} -> {new_path}")
-
-# Call the function to copy and rename files
-copy_and_rename_files(source_dir, destination_dir)
+print("Copying and renaming completed.")
