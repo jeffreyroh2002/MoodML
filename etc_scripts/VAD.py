cae4f09a39db6515188f542ebbcfbb6d3153fb8d
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-09-25 06:39:23 +0000

    adjusted VAD min silence to 10 secs

--- etc_scripts/VAD.py
+++ etc_scripts/VAD.py
@@ -3,7 +3,7 @@ from pydub.silence import split_on_silence
 import sys
 import os
 
-def remove_empty_vocal(input_audio_path, output_audio_path, min_silence_duration=1000):
+def remove_empty_vocal(input_audio_path, output_audio_path, min_silence_duration=10000):
     # Load the input vocal audio file
     audio = AudioSegment.from_file(input_audio_path, format="wav")
 
@@ -20,9 +20,9 @@ def remove_empty_vocal(input_audio_path, output_audio_path, min_silence_duration
 
 if __name__ == "__main__":
     input_audio_path = "dataset_lyrics_extracted/Vocals/Relaxed/백예린 (Yerin Baek) - 0310  가사_01_vocals.wav"
-    output_audio_path = "cleaned_audio.wav"
+    output_audio_path = "yerin_cleaned_audio_10000.wav"
 
     # Specify the minimum duration of silence to consider as empty vocal (in milliseconds)
-    min_silence_duration = 500  # Adjust this value as needed
+    min_silence_duration = 10000  # Adjust this value as needed
 
     remove_empty_vocal(input_audio_path, output_audio_path, min_silence_duration)
