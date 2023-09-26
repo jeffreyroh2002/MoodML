80d04d89e2111e412355c1a5abb0679b7c0540e8
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-09-26 12:24:30 +0000

    updated path for timbre json file

--- etc_scripts/save_mfcc.py
+++ etc_scripts/save_mfcc.py
@@ -3,8 +3,8 @@ import os
 import math
 import librosa
 
-DATASET_PATH = "dataset_lyrics_extracted/Background"
-JSON_FILE_NAME = "dataset_lyrics_extracted/background_dataset.json"
+DATASET_PATH = "../vocal_timbre_analysis/yt_dataset/voice_original"
+JSON_FILE_NAME = "../vocal_timbre_analysis/yt_dataset/eth_ras_smo_vocal.json"
 JSON_PATH = JSON_FILE_NAME
 
 SAMPLE_RATE = 22050
