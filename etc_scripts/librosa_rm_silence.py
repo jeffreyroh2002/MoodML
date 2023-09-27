ec5b355f7eef94555349c51946dfb55777bcb0b4
Jeffrey Roh <jeffreyroh2002@gmail.com>
2023-09-27 06:30:40 +0000

    testing librosa version instead of pydub

---
+++ etc_scripts/librosa_rm_silence.py
@@ -0,0 +1,26 @@
+import librosa
+import soundfile as sf
+
+# Load the input WAV file using librosa
+input_file = "../vocal_timbre_analysis/yt_dataset/voice_original/smo_vocal/I-vocals.wav"
+y, sr = librosa.load(input_file, sr=None)
+
+# Specify the minimum silence duration in seconds
+min_silence_duration = 3  # 3 seconds
+
+# Detect silent regions based on energy threshold
+silent_regions = librosa.effects.split(y, top_db=-22, frame_length=2048, hop_length=512)
+
+# Extract non-silent segments
+non_silent_audio = []
+for start, end in silent_regions:
+    non_silent_audio.extend(y[start:end])
+
+# Convert the non-silent audio to a numpy array
+non_silent_audio = librosa.util.fix_length(np.array(non_silent_audio), None)
+
+# Save the resulting audio to a new WAV file using soundfile
+output_file = "../lib_I-output.wav"
+sf.write(output_file, non_silent_audio, sr)
+
+print("Silence longer than", min_silence_duration, "seconds removed and saved as", output_file)
